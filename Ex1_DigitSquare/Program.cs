09262f4584527d82f748fe61518ff8dc035bba00
Artem <artem2015-asd@yandex.ru>
2022-08-31 20:23:43 +0300

    square

---
+++ Ex1_DigitSquare/Program.cs
@@ -0,0 +1,16 @@
+﻿
+Console.WriteLine("Введите число ");
+int firstDigit = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число, которое является квадратом первого введенного числа ");
+int secondDigit = Convert.ToInt32(Console.ReadLine());
+
+int square = firstDigit*firstDigit;
+
+if (secondDigit == square)
+{
+    Console.WriteLine ("Второе число является квадратом первого числа");
+}
+else Console.WriteLine ("Второе число не является квадратом первого числа");
+
+
+ 
