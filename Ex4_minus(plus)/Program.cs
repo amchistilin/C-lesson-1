7d562fd12e55e8b13d03605fbad0d26994e55ae7
Artem <artem2015-asd@yandex.ru>
2022-08-31 20:06:12 +0300

    plus (minus) exercise

---
+++ Ex4_minus(plus)/Program.cs
@@ -0,0 +1,16 @@
+﻿
+Console.WriteLine("Введите целое число");
+int digit = Convert.ToInt32(Console.ReadLine());
+try
+    {
+    int i = - digit;
+    while (i<=digit)
+            {
+                Console.Write (i + ", ");
+                i++;
+            }
+    }
+catch 
+{
+    Console.WriteLine("Надо было вводить именно целое число");
+}
